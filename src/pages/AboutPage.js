69a19f03b42b4b76417e9b32785a309c77c7980e
Leo Hoedl <leo.hoedl@yahoo.com>
2020-10-15 00:39:35 -0500

    Changing About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,14 +9,13 @@ function AboutPage(props){
             <HomeHeader title={props.title}/>
 
             <Content>
-                <p>How many programmers does it take to change a light bulb? None, that's a hardware problem.</p>
+                <p>I am a Computer Engineer specializing in Software Engineering with a background in computer architecture, embedded systems, and web development. 
+                    Relevant experience includes advanced digital design with VHDL, work with embedded systems on a PIC micro-controller using MPLAB X and C, wireless protocols, 
+                    and in depth knowledge of operating systems and computer architecture. 
+                    Over the years I have honed my talents of advanced software design with a mix of front-end and back-end programming languages including Java, C#, C, C++, VisualBasic, Python, JavaScript, LabVIEW, VHDL, React, and HTML. 
+                    My mix of programming background and in-depth knowledge of operating systems, computer architecture, and computer protocols can bring a unique viewpoint to any problem.</p>
 
-                <p>Fortunately for me I have experience with both! I am a Computer Engineer specializing in Software Engineering with a background in hardware design
-                and embedded systems. Over the years I have honed my talents with a mix in front end and back end programming languages including
-                Java, C#, C, C++, VisualBasic, Python, JavaScript, React, and HTML. My mix of programming background
-                and in-depth knowledge of operating systems, computer architecture, and computer protocols can bring a unique viewpoint to any problem.</p>
-
-                <p>When I'm not on the job I love hiking and camping</p>  
+                <p>When I'm not on the job I love hiking and camping.</p> 
             </Content>
         </div>
     );
