2a5e68667792906427ed6d68392d402ceae7880b
Leo Hoedl <leo.hoedl@yahoo.com>
2020-10-15 00:46:53 -0500

    Chaning footer to include copyright notice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
                 <Containter fluid="true">
                     <Row className="border-top justify-content-between p-3"> 
                         <Col className="p-2" md={3} small={12}>
-                            Leo Hoedl © 2020
+                            Copyright © 2020 Leo Hoedl
                         </Col>
                         <Col className="p-2 d-flex justify-content-end" md={3}>
                             This site was made by Leo Hoedl
